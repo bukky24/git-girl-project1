7c418cc84a92ed93187e89f2b8cf9c566bdf2ed7
bukky24 <46581093+bukky24@users.noreply.github.com>
2019-01-24 15:12:12 +0100

    Add files via upload

--- Module4_2.py
+++ Module4_2.py
@@ -1,6 +1,22 @@
 # This program convert the dictionary Fin_status to Panda series
+import numpy as np
 import pandas as pd
-Fin_status = {'Jan': 25, 'Feb': 191, 'Mar': 738, 'Apr': 797, 'May': -60, 'Jun': -30, 'Jul': -214, 'Aug': 394, 'Sep': -594, 'Oct': -23, 'Nov': 7, 'Dec': -226}
 
+months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+revenue = [145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154]
+expenses = [120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380]
+
+# The list is converted to numpy array
+m = months
+r = revenue
+e = expenses
+m = np.array(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+r = np.array([145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154])
+e = np.array([120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380])
+
+profit = np.subtract(r, e)
+Fin_status = dict(zip(m ,profit))
+
+# Converts the dictionary to Pandas Series
 P_series = pd.Series(Fin_status)
 print(P_series)
