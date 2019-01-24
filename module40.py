0997e710eca91fb6dd969580ce96fb54aa748b25
bukky24 <46581093+bukky24@users.noreply.github.com>
2019-01-24 14:51:02 +0100

    Add files via upload

--- module40.py
+++ module40.py
@@ -1,4 +1,5 @@
 # This program calculates the profit for ABD company for a year
+
 import numpy as np
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 revenue = [145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154]
@@ -14,4 +15,4 @@ e = np.array([120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380])
 
 # Profit is calculated as Revenue minus Expenses
 profit = np.subtract(r, e)
-print ("The Profit is", profit)
+print ("The Profit is \n", profit)
