5bbbaa95639d5c6a0ca6b4bc9672706d3c7faec3
bukky24 <46581093+bukky24@users.noreply.github.com>
2019-01-24 14:36:45 +0100

    Add files via upload

--- Module4_1.py
+++ Module4_1.py
@@ -1,8 +1,22 @@
 # This program extract data from months and profit into Fin_status
 
+import numpy as np
 months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-profit = [  25,  191,  738,  797,  -60,  -30, -214,  394, -594,  -23, 7, -226]
+revenue = [145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154]
+expenses = [120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380]
 
-Fin_status = dict(zip(months,profit))
+# The list is converted to numpy array
+m = months
+r = revenue
+e = expenses
+m = np.array(['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'])
+r = np.array([145, 760, 861, 917, 805, 810, 114, 976, 103, 143, 107, 154])
+e = np.array([120, 569, 123, 120, 865, 840, 328, 582, 697, 166, 100, 380])
+
+# Profit is calculated as Revenue minus Expenses
+profit = np.subtract(r, e)
+
+# Extract data from month and profit into a dictionary to be named ‘Fin_status’
+Fin_status = dict(zip(m ,profit))
 print (Fin_status)
  
